2e7ca3cb3af19f4f5965bd6c956011aa536acd60
khaled <khmeltazy@gmail.com>
2025-04-24 21:36:11 +0200

    updaing compileSdk to 35

--- app/src/main/java/com/android/jetpackcomposelandofcoding/MainActivity.kt
+++ app/src/main/java/com/android/jetpackcomposelandofcoding/MainActivity.kt
@@ -3,12 +3,10 @@ package com.android.jetpackcomposelandofcoding
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdgegi
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
 
         }
