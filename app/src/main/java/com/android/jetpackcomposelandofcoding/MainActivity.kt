ce9a7188c52a82801a66e14df7917d09f034bcaa
khaled <khmeltazy@gmail.com>
2025-04-25 22:17:40 +0300

    learning about textField

--- app/src/main/java/com/android/jetpackcomposelandofcoding/MainActivity.kt
+++ app/src/main/java/com/android/jetpackcomposelandofcoding/MainActivity.kt
@@ -3,12 +3,92 @@ package com.android.jetpackcomposelandofcoding
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                var text by remember {
+                    mutableStateOf("")
+                }
 
+                var isPasswordVisible by remember{
+                    mutableStateOf(false)
+                }
+
+                val focusRequester = remember {
+                    FocusRequester()
+                }
+
+                OutlinedTextField(
+                    value = text,
+                    onValueChange = { text = it },
+                    leadingIcon = {
+                        Icon(painter = painterResource(R.drawable.baseline_key_24), contentDescription = null)
+                    },
+                    trailingIcon = {
+                        TextButton(onClick = {isPasswordVisible = !isPasswordVisible}) {
+                            Text(text = if(isPasswordVisible) "Hide" else "Show")
+                        }
+                    },
+//                    colors = TextFieldDefaults.textFieldColors(
+//                        containerColor = Color.Transparent,
+//                        cursorColor = Color.Red
+//                    ),
+                    singleLine = true,
+                    placeholder = {
+                        Text("Password")
+                    },
+//                    label = {
+//                        Text("Password")
+//                    }
+                    visualTransformation = if(isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(
+                        autoCorrect = false,
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Go
+                    ),
+                    modifier = Modifier
+                        .focusRequester(focusRequester)
+                )
+
+                Button(onClick = {focusRequester}) {
+                    Text("Request Focus")
+                }
+            }
         }
     }
 }
