6c627d8eeba7507fd108e9616af90f73acf25fec
khaled <khmeltazy@gmail.com>
2025-04-26 01:41:02 +0300

    desing loginScreen

--- app/src/main/java/com/android/jetpackcomposelandofcoding/MainActivity.kt
+++ app/src/main/java/com/android/jetpackcomposelandofcoding/MainActivity.kt
@@ -3,12 +3,15 @@ package com.android.jetpackcomposelandofcoding
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.android.jetpackcomposelandofcoding.ui.theme.ComposeLoginScreenInitTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            ComposeLoginScreenInitTheme{
+                LoginScreen()
+            }
         }
     }
 }
