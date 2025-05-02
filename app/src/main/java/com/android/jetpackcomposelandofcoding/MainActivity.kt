dbb577e0d35784db0fbff5784638a19012e1cfe9
khaled <khmeltazy@gmail.com>
2025-05-03 02:26:55 +0300

    transitions

--- app/src/main/java/com/android/jetpackcomposelandofcoding/MainActivity.kt
+++ app/src/main/java/com/android/jetpackcomposelandofcoding/MainActivity.kt
@@ -3,6 +3,12 @@ package com.android.jetpackcomposelandofcoding
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.ui.unit.IntOffset
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,7 +23,19 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             NavHost(navController = navController, startDestination = "screen_a"){
                 composable(
-                    route = "screen_a"
+                    route = "screen_a",
+                    exitTransition = {
+                        slideOutOfContainer(
+                            AnimatedContentTransitionScope.SlideDirection.Left,
+                            tween(1000)
+                        )
+                    },
+                    popEnterTransition = {
+                        slideIntoContainer(
+                            AnimatedContentTransitionScope.SlideDirection.Right,
+                            tween(1000)
+                        ) + fadeIn()
+                    }
                 ){
                     ScreenA(navigateToB = {
                         navController.navigate("screen_b")
@@ -25,7 +43,19 @@ class MainActivity : ComponentActivity() {
                 }
 
                 composable(
-                    route = "screen_b"
+                    route = "screen_b",
+                    enterTransition = {
+                        slideIntoContainer(
+                            AnimatedContentTransitionScope.SlideDirection.Left,
+                            tween(1000)
+                        )
+                    },
+                    popExitTransition = {
+                        slideOutOfContainer(
+                            AnimatedContentTransitionScope.SlideDirection.Right,
+                            tween(1000)
+                        ) + fadeOut()
+                    }
                 ){
                     ScreenB(
                         navigateToC = {
