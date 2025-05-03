028c938e2cfc6df0cd639348537031e6d51b4926
khaled <khmeltazy@gmail.com>
2025-05-03 03:47:35 +0300

    shared view model

--- app/src/main/java/com/android/jetpackcomposelandofcoding/MainActivity.kt
+++ app/src/main/java/com/android/jetpackcomposelandofcoding/MainActivity.kt
@@ -6,37 +6,48 @@ import androidx.activity.compose.setContent
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.android.jetpackcomposelandofcoding.screens.Person
 import com.android.jetpackcomposelandofcoding.screens.ScreenA
 import com.android.jetpackcomposelandofcoding.screens.ScreenB
+import com.android.jetpackcomposelandofcoding.screens.SharedViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = "screenA"){
-                composable(
-                    route = "screenA"
+            NavHost(navController = navController, startDestination = "main") {
+                navigation(
+                    route = "main",
+                    startDestination = "screenA"
                 ) {
-                    ScreenA(navigateToB = {
-                        navController.currentBackStackEntry?.savedStateHandle?.set(
-                            "person",it
-                        )
-                        navController.navigate("screenB")
-                    })
+                    composable(
+                        route = "screenA"
+                    ) {
+                        val viewModel: SharedViewModel =
+                            it.SharedViewModel(navController = navController)
+                        ScreenA(navigateToB = {
+                            viewModel.person = it
+                            navController.navigate("screenB")
+                        })
+                    }
+
+                    composable(
+                        route = "screenB"
+                    ) {
+                        val viewModel: SharedViewModel = it.SharedViewModel(navController)
+                        viewModel.person?.let {
+                            ScreenB(it)
+                        }
+
+                    }
                 }
 
-                composable(
-                    route = "screenB"
-                ) {
-                    navController.previousBackStackEntry?.savedStateHandle?.get<Person>("person")?.let {
-                        ScreenB(it)
-                    }                }
-
             }
+
         }
     }
 }
---
+++ app/src/main/java/com/android/jetpackcomposelandofcoding/screens/SharedViewModel.kt
@@ -0,0 +1,7 @@
+package com.android.jetpackcomposelandofcoding.screens
+
+import androidx.lifecycle.ViewModel
+
+class SharedViewModel : ViewModel() {
+    var person : Person? = null
+}
